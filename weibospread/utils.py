4acf2a85b82c6a310117c172964243bcdb9cb507
MOON_CLJ <lijunli2598@gmail.com>
2013-04-02 09:47:21 +0800

    little fix

--- weibospread/utils.py
+++ weibospread/utils.py
@@ -27,16 +27,16 @@ def require_login(f):
     return decorated
 
 #prod
-'''
 APP_KEY = '4131380600'
 APP_SECRET = 'df544af4a9e30abe16e715cb4d0be423'
 CALLBACK_URL = 'http://idec.buaa.edu.cn:8080/callback'
-'''
 
 #dev
+"""
 APP_KEY = '1966311272'
 APP_SECRET = '57d36e0eaef033593f4bb6f745a67c5f'
 CALLBACK_URL = 'http://127.0.0.1:8080/callback'
+"""
 
 
 def get_client(access_token=None, expires_at=None):
